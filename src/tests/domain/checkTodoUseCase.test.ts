1c8136ba8c427315414fd5837fe860747ce71ce3
cyrille <aviateur22@hotmail.fr>
2023-02-21 10:38:08 +0100

    Complete CheckTodo Usecase

--- src/tests/domain/checkTodoUseCase.test.ts
+++ src/tests/domain/checkTodoUseCase.test.ts
@@ -1,5 +1,6 @@
 import { CheckToggleTodoEntity } from "../../domain/entities/todo/CheckToggleTodoEntity";
 import { UseCaseServiceImpl } from "../../domain/services/UseCaseServiceImpl";
+import { TodoNotFindException } from "../../exceptions/TodoNotFindException";
 import { TodoGenerator } from "./utilities/TodoGenerator";
 
 describe('UseCase: getAllTodos', () => {
@@ -31,6 +32,40 @@ describe('UseCase: getAllTodos', () => {
       
     } catch (error) {
       expect(error).toBeFalsy();
+    }    
+  });
+
+  // Uncheck Todo
+  it('Should uncheck complete Todo', async() => {
+    try {
+      const checkToggleTodo = new CheckToggleTodoEntity('1', false);
+
+      // Uncheck todo
+      const todo = await checkTodoUseCase.execute(checkToggleTodo);
+
+      // Récupération todo modifié
+      const findTodo = await UseCaseServiceImpl.getUseCases().findOneTodoUseCase.execute(checkToggleTodo);
+
+      expect(todo.status).toBeFalsy();
+      expect(findTodo.id).toBe(checkToggleTodo.id);
+      
+    } catch (error) {
+      expect(error).toBeFalsy();
+    }    
+  });
+
+  // Check Todo impossible car Todo non existante
+  it('Should throw TodoNotfindException because Todo does not exist', async() => {
+    try {
+      const checkToggleTodo = new CheckToggleTodoEntity('3', true);
+
+      // Check todo
+      const todo = await checkTodoUseCase.execute(checkToggleTodo);
+
+      expect(todo).toBeFalsy();
+      
+    } catch (error) {
+      expect(error).toBeInstanceOf(TodoNotFindException);
     }    
   });
 
