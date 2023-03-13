aa365720a12e9524f5d4be63c1f388a75a13f17f
cyrille <aviateur22@hotmail.fr>
2023-03-13 23:33:30 +0100

    start implement ajv error

--- src/infra/frameworks/server/fastify/app/middlewares/validations/todo/index.ts
+++ src/infra/frameworks/server/fastify/app/middlewares/validations/todo/index.ts
@@ -4,6 +4,7 @@ import updateTodoSchema from "./updateTodoSchema";
 import checkToggleTodoSchema from "./checkToggleTodoSchema";
 import deleteTodoSchema from "./deleteTodoSchema";
 import findOneTodoSchema from "./findOneTodoSchema";
+import test from "./test";
 
 // Export des schéma de vérification des todos
 export default {
@@ -12,5 +13,6 @@ export default {
   updateTodoSchema,
   checkToggleTodoSchema,
   deleteTodoSchema,
-  findOneTodoSchema
+  findOneTodoSchema,
+  test
 }
--- src/infra/frameworks/server/fastify/app/router/api/todo.ts
+++ src/infra/frameworks/server/fastify/app/router/api/todo.ts
@@ -1,4 +1,5 @@
 import { fastify, FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+
 import todos from "../../controllers/todos";
 import todoSchemaValidation from '../../middlewares/validations/todo';
 import  Ajv from 'ajv'
@@ -31,14 +32,15 @@ export default async function todo(fastify: FastifyInstance, options: Object ) {
   fastify.get('/find-all-todos', todoSchemaValidation.findAllTodosSchema,  todos.findAllTodos);
 
   // Save 1 Todo
-  fastify.post('/', todoSchemaValidation.addTodoSchema, todos.saveOneTodo);
+  // fastify.post('/', todoSchemaValidation.addTodoSchema, todos.saveOneTodo);
 
   // Findone Todo
   fastify.get('/:id', todoSchemaValidation.findOneTodoSchema, todos.findOneTodo);
-
-  // // Save Todo
-  // fastify.post('/' , { schema: {body: todoSchemaValidation.addTodoSchema}, validatorCompiler: ({schema})=>{
-  //    const validate = ajv.compile(schema)
-  //     return validate
-  // }} ,todos.saveOneTodo);
+  const schemaProperty = todoSchemaValidation.test;
+  
+  // Save Todo
+  fastify.post('/' , {  schema: todoSchemaValidation.test, validatorCompiler: ({schema})=>{
+     const validate = ajv.compile(schema)
+      return validate
+  }} ,todos.saveOneTodo);
 }
